1612dbbe6d234ef4c2e75edb8b9187c989a0de33
cxy <384171694@qq.com>
2023-04-02 16:40:13 +0800

    back

--- utils/saveAdminUser.js
+++ utils/saveAdminUser.js
@@ -1,4 +1,4 @@
-// const CryptoJs = require('crypto-js')
+const CryptoJs = require('crypto-js')
 const { User } = require('../db/connect')
 
 module.exports = async function () {
@@ -7,7 +7,7 @@ module.exports = async function () {
 
     await User.create({
       account: 'admin',
-      // password: CryptoJs.MD5('123456').toString(),
+      password: CryptoJs.MD5('123456').toString(),
       password: '123456',
       username: 'Admin',
       role: '总领导',
